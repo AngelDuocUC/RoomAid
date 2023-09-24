49461f915628d8f44e4abcf8e666a96addda7d71
DeltasPrime <nico.vasquezb@duocuc.cl>
2023-09-23 22:45:59 -0300

    1.3
    Css de Home y Perfil pt2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,5 +10,22 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonicModule, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+  public buffer = 0.06;
+  public progress = 0;
+  constructor() {
+    setInterval(() => {
+      this.buffer += 0.06;
+      this.progress += 0.06;
+
+      // Reset the progress bar when it reaches 100%
+      // to continuously show the demo
+      if (this.progress > 1) {
+        setTimeout(() => {
+          this.buffer = 0.06;
+          this.progress = 0;
+        }, 1000);
+      }
+    }, 1000);
+  }
+
 }
